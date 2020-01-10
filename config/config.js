38d1f8cd64ece2a7896ff1c319c6296fdb680789
hystweb <hystweb@digi.com>
2020-01-10 18:14:50 +0800

    first commit

--- config/config.js
+++ config/config.js
@@ -2,11 +2,31 @@ export default {
     singular: true,
     plugins: [
       ['umi-plugin-react', {
-        // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
+        antd: true,
+        dva: true
       }],
     ],
     routes: [{
-        path: '/',
-        component: './HelloWorld',
-      }],
+      path: '/',
+      component: '../layout',
+      routes: [
+        {
+          path: '/',
+          component: './helloworld',
+        },
+        {
+          path: '/helloworld',
+          component: './helloworld'
+        },
+        {
+          path: '/dashboard',
+          routes: [
+            { path: '/dashboard/analysis', component: './Dashboard/Analysis' },
+            { path: '/dashboard/monitor', component: './Dashboard/Monitor' },
+            { path: '/dashboard/workplace', component: './Dashboard/Workplace' }
+          ]
+        },
+        { path: 'puzzlecards', component: './puzzlecards' },
+      ]
+    }]
 };
